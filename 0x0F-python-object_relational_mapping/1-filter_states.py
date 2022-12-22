98161dbb95b8cafea60d176f25268fbf511998f5
Ajgesh <40598406+Ajgesh@users.noreply.github.com>
2022-12-23 00:06:56 +0100

    Create 1-filter_states.py

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Lists all states from the database hbtn_0e_0_usa with a name
+    starting with N"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    database = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                               passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    curs = database.cursor()
+    curs.execute("""SELECT * FROM states WHERE name
+                 LIKE BINARY 'N%' ORDER BY states.id""")
+    rows = curs.fetchall()
+    for row in rows:
+        print(row)
+    curs.close()
+    database.close()
