298cbf4d9ec2ca7f19933bc9ba10e4493a6e6bab
Ajgesh <40598406+Ajgesh@users.noreply.github.com>
2022-12-23 00:23:09 +0100

    Create 5-filter_cities.py

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Lists all states from the database hbtn_0e_0_usa with a name"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    database = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                               passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    curs = database.cursor()
+    curs.execute("""SELECT cities.name FROM
+                 cities INNER JOIN states ON states.id=cities.state_id
+                 WHERE states.name=%s""", (sys.argv[4],))
+    rows = curs.fetchall()
+    tmp = list(row[0] for row in rows)
+    print(*tmp, sep=", ")
+    curs.close()
+    database.close()
