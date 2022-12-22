b842762225d11030b5bda9af985c7aff9b0646a0
Ajgesh <40598406+Ajgesh@users.noreply.github.com>
2022-12-23 00:19:35 +0100

    Create 3-my_safe_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Lists all states from the database hbtn_0e_0_usa """
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    database = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                               passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    curs = database.cursor()
+    match = sys.argv[4]
+    curs.execute("SELECT * FROM states WHERE name LIKE %s", (match, ))
+    rows = curs.fetchall()
+    for row in rows:
+        print(row)
+    curs.close()
+    database.close()
